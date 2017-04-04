c100d3e5fe2b417535c49a75f574e2959bf7cb6e
Joseph Mangmang <joseph.deplacer@gmail.com>
2017-04-04 13:32:53 +0800

    change is_diver to account_type

--- app/src/main/java/com/divetym/dive/ui/models/User.java
+++ app/src/main/java/com/divetym/dive/ui/models/User.java
@@ -13,26 +13,32 @@ public class User {
     private String email;
     @SerializedName("create_time")
     private String createdTime;
-    @SerializedName("is_diver")
-    private boolean isDiver;
+    @SerializedName("account_type")
+    private UserType accountType;
     @SerializedName("auth_key")
     private String authKey;
+
+    public enum UserType {
+        @SerializedName("diver")
+        Diver,
+        @SerializedName("dive_shop")
+        DiveShop
+    }
 
     public User() {
     }
 
-    public User(int userUid, String email, boolean isDiver, String authKey) {
+    public User(int userUid, String email, String authKey) {
         this.userUid = userUid;
         this.email = email;
-        this.isDiver = isDiver;
         this.authKey = authKey;
     }
 
-    public User(int userUid, String email, String createdTime, boolean isDiver) {
+    public User(int userUid, String email, String createdTime, UserType accountType) {
         this.userUid = userUid;
         this.email = email;
         this.createdTime = createdTime;
-        this.isDiver = isDiver;
+        this.accountType = accountType;
     }
 
     public int getUserUid() {
@@ -59,19 +65,19 @@ public class User {
         this.createdTime = createdTime;
     }
 
-    public boolean isDiver() {
-        return isDiver;
-    }
-
-    public void setDiver(boolean diver) {
-        isDiver = diver;
-    }
-
     public String getAuthKey() {
         return authKey;
     }
 
     public void setAuthKey(String authKey) {
         this.authKey = authKey;
+    }
+
+    public UserType getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(UserType accountType) {
+        this.accountType = accountType;
     }
 }
